265a3a1e57bb7c147faa49db237fb1d108e40ecd
leo rentmeister <leo.wehrle@epitech.eu>
2023-10-17 00:30:38 +0200

    remove: version.h

--- include/version.h
+++
@@ -1,5 +0,0 @@
-#ifndef VERSION_H
-    #define VERSION_H
-    #define VERSION_GIT  "ea7ce0a4b7607e4ad1abca6b9a41c1d97c63b86b*"
-    #define VERSION_DATE "16-Oct-2023 21h:33m:52s (UTC)"
-#endif /* VERSION_H */
